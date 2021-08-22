28aeb893c2d48e88dad5b10a11c09f752e439f86
Artem Stemkovski <astemkov@gmail.com>
2021-08-22 13:12:40 -0700

    case-fold-search nil

--- init.el
+++ init.el
@@ -136,6 +136,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(case-fold-search nil)
  '(espresso-indent-level 2)
  '(indent-tabs-mode nil)
  '(js-curly-indent-offset 2)
